c0a2533901b42dae98a19c10cd5de5aa290fe6d9
Mingyue Li <3249977074@qq.com>
2023-12-23 16:55:49 +0800

    chore: separate lisp scripts

---
+++ lisp/init-elpa.el
@@ -0,0 +1,28 @@
+(setq package-archives '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
+                         ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+
+
+(setq package-check-signature nil)
+
+(require 'package)
+
+(unless (bound-and-true-p pacakge-initialized)
+  (package-initialize))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(setq use-package-always-ensure t
+      use-package-always-defer t
+      use-package-always-demand t
+      use-package-expand-minimal t
+      use-package-verbose t)
+
+(require 'use-package)
+
+(provide 'init-elpa)
