24083cacb921dc1bbf28d17b45964fbdc360bc14
corallee <corallee9797@gmail.com>
2024-01-03 16:52:59 +0800

    feat: config with org

--- init.el
+++ init.el
@@ -1,15 +1,6 @@
-(add-to-list 'load-path
-	     (expand-file-name (concat user-emacs-directory "lisp")))
-
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-
-(require 'init-const)
-(require 'init-keyboard)
-(require 'init-setup)
-(require 'init-elpa)
-(require 'init-packages)
-(require 'init-ui)
-(require 'init-lsp)
-
-(when (file-exists-p custom-file)
-  (load-file custom-file))
+(setq package-install-upgrade-built-in t)
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
